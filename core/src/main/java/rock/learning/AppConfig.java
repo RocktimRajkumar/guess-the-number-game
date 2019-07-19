1c6bb90ef522e213605e1cdbb76de942d273180a
Rocktim <rajkumar.rocktim@gmail.com>
2019-07-19 08:10:36 +0530

    beans.xml : deleted convert xml base to annotation base config

---
+++ core/src/main/java/rock/learning/AppConfig.java
@@ -0,0 +1,9 @@
+package rock.learning;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "rock.learning")
+public class AppConfig {
+}
