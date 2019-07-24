251b84d1a1d3e0bd5777ca49155b0c0d643ce280
Rocktim <rajkumar.rocktim@gmail.com>
2019-07-24 09:19:49 +0530

    console > java > Main : code cleanup done

--- console/src/main/java/rock/learning/console/Main.java
+++ console/src/main/java/rock/learning/console/Main.java
@@ -1,12 +1,10 @@
-package src.main.java.rock.learning.console;
+package rock.learning.console;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import rock.learning.config.AppConfig;
-import rock.learning.MessageGenerator;
-import rock.learning.NumberGenerator;
 
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
@@ -16,21 +14,6 @@ public class Main {
 
         // create context (container)
         ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-
-        // get numberGenerator bean from context (container)
-        NumberGenerator numberGenerator = context.getBean(NumberGenerator.class);
-
-        // call method next() to get a random number
-        int number = numberGenerator.next();
-
-        // log generated number
-        log.info(" number = {}", number);
-
-        // get messageGenerator bean from context (container)
-        MessageGenerator messageGenerator = context.getBean(MessageGenerator.class);
-
-        log.info("getMainMessage = {}", messageGenerator.getMainMessage());
-        log.info("getResultMessage = {}", messageGenerator.getResultMessage());
 
         // close context (container)
         context.close();
