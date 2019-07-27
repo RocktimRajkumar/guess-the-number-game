346417e3b937bc9ecd0d5b9b017b498be3abbfaf
Rocktim <rajkumar.rocktim@gmail.com>
2019-07-27 12:54:48 +0530

    console > java > consoleNumberGuess : Lombok slf4j implement done

--- console/src/main/java/rock/learning/console/ConsoleNumberGuess.java
+++ console/src/main/java/rock/learning/console/ConsoleNumberGuess.java
@@ -1,5 +1,6 @@
 package rock.learning.console;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +12,9 @@ import rock.learning.MessageGenerator;
 
 import java.util.Scanner;
 
+@Slf4j
 @Component
 public class ConsoleNumberGuess {
-
-    // ==== constants ====
-    private static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
 
     // ==== fields ====
     private final Game game;
